769955d916d4c3a37b94a4d57ed28c8c7d11e098
June_nine <09jhc1997@gmail.com>
2021-03-17 18:32:45 +0900

    sujeong 2nd week assignment

--- src/main.java
+++ src/main.java
@@ -1,8 +1,28 @@
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.lang.String;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 public class main {
+    public String[] htmltag(File fileName) throws IOException{
+        String [] strtag = new String[2];
+        try{
+            Document doc = Jsoup.parse(fileName, "UTF-8");
+            Elements title = doc.select("title");
+            strtag[0] = title.text();
+            Elements p = doc.select("p");
+            strtag[1] = p.text();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return strtag;
+    }
     public static void main(String[] args) throws ParserConfigurationException{
-        mkXml mXml = new mkXml();
-        mXml.htmltoXml();
+
+        mkXml createXml = new mkXml();
+        createXml.htmltoXml();
     }
 }
--- src/mkXml.java
+++ src/mkXml.java
@@ -10,41 +10,38 @@ import java.io.File;
 import java.io.FileOutputStream;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-
-
+import java.lang.String;
 
 public class mkXml      {
     public void htmltoXml()throws ParserConfigurationException{
-        htmlparser mkp = new htmlparser();
-        File [] htmlsdir = mkp.htmlfiles();
-        int htmlnum = 5;
-
+        main mkp = new main();
         try{
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            Document document = db.newDocument();
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            Document document = docBuilder.newDocument();
             Element docs = document.createElement("docs");
             document.appendChild(docs);
+            int htmlnum = 5;
+            File loc = new File("./2주차 실습 html");
+            File htmls[] = loc.listFiles();
             for(int i = 0; i <htmlnum; i++){
-                String [] gettag = mkp.htmltag(htmlsdir[i]);
-                String Xmltitle = gettag[0];
-                String Xmlp = gettag[1];
+                String [] gettag = mkp.htmltag(htmls[i]);
                 Element doc = document.createElement("doc");
                 docs.appendChild(doc);
                 String num = Integer.toString(i);
                 doc.setAttribute("id", num);
                 Element title =  document.createElement("title");
-                title.appendChild(document.createTextNode(Xmltitle));
+                title.appendChild(document.createTextNode(gettag[0]));
                 Element body = document.createElement("body");
                 doc.appendChild(body);
-                body.appendChild(document.createTextNode(Xmlp));
+                body.appendChild(document.createTextNode(gettag[1]));
             }
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
             transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-            DOMSource src = new DOMSource(document);
+            DOMSource source = new DOMSource(document);
             StreamResult result = new StreamResult(new FileOutputStream(new File("./collection.xml")));
-            transformer.transform(src, result);
+            transformer.transform(source, result);
 
         }catch(Exception e){
             System.out.println(e);
