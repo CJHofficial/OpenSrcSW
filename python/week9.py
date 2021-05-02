17eb4ae99309163e9180a8776e19f5cc5a2cfcf7
June_nine <09jhc1997@gmail.com>
2021-05-02 17:16:43 +0900

    python 9주차 실습 폴더추가

---
+++ python/week9.py
@@ -0,0 +1,8 @@
+x = float(input("x값을 입력하세요 : "))
+y= 3*x*x + 5*x + 3
+print("y값은 ", y,"입니다.\n")
+
+r = float(input("반지름을 입력하세요 : "))
+cir = 3.14*2*r
+s = 3.14*r*r
+print("원의 둘레 : ",cir," 원의 넓이 : ",s)
